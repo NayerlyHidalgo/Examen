f1a9b39da75f429bf539a6a22125cd8c09493101
Nayerlie hidalgo <melifer.j1995@gmail.com>
2025-07-13 16:11:48 -0500

    feat: added logs folder

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Esta es una app hecha con los Genios de Programación III';
+    return 'Tatoo Store API is running! Welcome to the Tatoo Store API! version: 2025.07.13.16.11';
   }
 }
